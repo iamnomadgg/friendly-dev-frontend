31de96c57cde5a4d4c2b04242edbc6a957d5bcda
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-07-28 16:33:42 +0200

    feat(projects): add project details page with clientLoader and fallback

---
+++ app/routes/projects/details.tsx
@@ -0,0 +1,26 @@
+import type { Route } from './+types/details';
+import type { Project } from '~/types';
+
+export async function clientLoader({
+  request,
+  params,
+}: Route.ClientLoaderArgs): Promise<Project> {
+  const res = await fetch(`http://localhost:5000/projects/${params.id}`);
+
+  if (!res.ok) throw new Response('Project Not Found', { status: 404 });
+
+  const project: Project = await res.json();
+  return project;
+}
+
+export function HydrateFallback() {
+  return <div>Loading...</div>;
+}
+
+const ProjectDetailsPage = ({ loaderData }: Route.ComponentProps) => {
+  const project = loaderData;
+  console.log(project);
+  return <>ProjectDetails</>;
+};
+
+export default ProjectDetailsPage;
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router';
 import type { Route } from './+types/index';
 import type { Project } from '~/types';
 import ProjectCard from '~/components/ProjectCard';
@@ -19,12 +18,7 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
       <h2 className="text-3xl text-white font-bold mb-8">Projects</h2>
       <div className="grid gap-6 sm:grid-cols-2">
         {projects.map((project) => (
-          <Link
-            className="block transform transition duration-300 hover:scale-[1.02]"
-            to={`/projects/${project.id}`}
-          >
-            <ProjectCard key={project.id} project={project} />
-          </Link>
+          <ProjectCard key={project.id} project={project} />
         ))}
       </div>
     </>
