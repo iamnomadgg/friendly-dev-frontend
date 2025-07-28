9c72121dd91273007a2cf5c7400d07c9f559d077
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-07-28 15:49:56 +0200

    refactor(projects): add Project type to loader and component props

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,13 +1,16 @@
 import type { Route } from './+types/index';
+import type { Project } from '~/types';
 
-export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch('http://localhost:5000/projects');
   const data = await res.json();
   return { projects: data };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData;
+  const { projects } = loaderData as { projects: Project[] };
   console.log(projects);
   return (
     <>
