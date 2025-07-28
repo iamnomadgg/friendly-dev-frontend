fc9cef172bc65a44af8492f12d5c67afa240dda4
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-07-28 15:42:18 +0200

    feat: fetch projects with loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,14 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch('http://localhost:5000/projects');
+  const data = await res.json();
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  console.log(projects);
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">Projects</h2>
