e22784c80ca1387b764a9496166db6998661a14b
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-07-29 15:51:25 +0200

    feat(blog): add pagination to blog post listing

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import type { Route } from './+types/index';
 import PostCard from '~/components/PostCard';
+import Pagination from '~/components/Pagination';
 import type { PostMeta } from '~/types';
 
 export async function loader({
@@ -16,13 +18,28 @@ export async function loader({
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
   const { posts } = loaderData;
+  //Calculate total pages
+  const postsPerPage = 10;
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+
+  //Get current page's projects
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+
   return (
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl text-white font-bold mb-8">Blog</h2>
-      {posts.map((post) => (
+      {currentPosts.map((post) => (
         <PostCard key={post.slug} post={post} />
       ))}
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </div>
   );
 };
