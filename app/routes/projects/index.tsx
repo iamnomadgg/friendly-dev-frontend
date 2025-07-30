0b606966e15637e4e2542f38dffb6ac1081a44bc
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-07-30 15:46:50 +0200

    feat(projects): refactor projects loader and types for Strapi API integration

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import type { Route } from './+types/index';
-import type { Project } from '~/types';
+import type { Project, StrapiProject, StrapiResponse } from '~/types';
 import ProjectCard from '~/components/ProjectCard';
 import Pagination from '~/components/Pagination';
 import { AnimatePresence, motion } from 'framer-motion';
@@ -8,9 +8,27 @@ import { AnimatePresence, motion } from 'framer-motion';
 export async function loader({
   request,
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
-  return { projects: data };
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/projects?populate=*`
+  );
+  const json: StrapiResponse<StrapiProject> = await res.json();
+
+  const projects = json.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    // image: item.image?.url
+    //   ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
+    //   : '/images/no-image.png',
+    image: `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`,
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured,
+  }));
+
+  return { projects };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
